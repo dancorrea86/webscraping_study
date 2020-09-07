d617a0157c3187287b788090fbe09df5ce7ad534
dancorrea86 <danielmacielcorrea@gmail.com>
2020-09-07 19:08:34 -0300

    searchpypi

--- mapIt.py
+++ mapIt.py
@@ -7,11 +7,9 @@ import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     # Get address from command line.
     address = ' '.join(sys.argv[1:])
-    print (address)
 else:
     # Get address from clipboad.
     address = pyperclip.paste()
-    print (address)
 
 webbrowser.open('https://www.google.com/maps/place/' + address)
 
---
+++ searchpypi.py
@@ -0,0 +1,20 @@
+#! python3
+# searchpypi.py - Opens several search results.
+
+import requests, sys, webbrowser, bs4
+print('Searching...') # display text downloading the search result page
+res = requests.get('https://pypi.org/search/?q='
+                    + ''.join(sys.argv[1:])) # Junta a URL com o argumento da chamada
+
+res.raise_for_status()
+
+# TODO: Retrieve top search result links
+soup = bs4.BeautifulSoup(res.text, 'html.parser') # pega os texto da pagina em html
+
+# TODO: Open a browser tab for each result
+linkElems = soup.select('.package-snippet') # Filtra os elementos com a classe "package-snippet" que são links
+numOpen = min(5, len(linkElems)) # Verifica se a quantidade de links é maior do que 5, se sim limita a quantidade a 5
+
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    webbrowser.open(urlToOpen)
